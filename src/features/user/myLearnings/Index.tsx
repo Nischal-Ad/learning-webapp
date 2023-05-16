f44acf7a736dfc3621fdafeb6aa31d3c65314b1e
Nischal-Ad <92177440+Nischal-Ad@users.noreply.github.com>
2023-05-16 16:12:33 +0545

    my learnings page completed

--- src/features/user/myLearnings/Index.tsx
+++ src/features/user/myLearnings/Index.tsx
@@ -1,7 +1,28 @@
-import React from 'react';
+import Section from '@Components/SectionWrapper';
+import { Grid } from '@mui/material';
+import { courses } from '@Data/Courses';
+import Helmet from '@Components/Helmet';
+import CourseCard from '@Components/Courses/Index';
 
 const Index = () => {
-	return <div>learnings</div>;
+	return (
+		<>
+			<Helmet title='My-Learnings' content='my courses' />
+			<Section id='my-learnings'>
+				<Grid container spacing={2}>
+					{courses.map((course, i) => {
+						return (
+							<>
+								<Grid item xs={12} sm={6} md={4} lg={3}>
+									<CourseCard courses={course} key={i} list={false} />
+								</Grid>
+							</>
+						);
+					})}
+				</Grid>
+			</Section>
+		</>
+	);
 };
 
 export default Index;
