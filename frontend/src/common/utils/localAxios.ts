e9c2bb502a9565abbddd6715c246543a9d386c9a
Nischal-Ad <92177440+Nischal-Ad@users.noreply.github.com>
2023-05-23 21:47:45 +0545

    connecting backend to frontend and enables cors

--- frontend/src/common/utils/localAxios.ts
+++ frontend/src/common/utils/localAxios.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const localAxios = axios.create({
-	baseURL: '',
+	baseURL: 'http://localhost:3000',
 	withCredentials: true,
 });
 
--- frontend/src/redux/reducer.ts
+++ frontend/src/redux/reducer.ts
@@ -1,8 +1,10 @@
 import authSlice from '@Slices/auth.slice';
+import userSlice from '@Slices/user.slice';
 import { combineReducers } from '@reduxjs/toolkit';
 
 const reducer = combineReducers({
 	auth: authSlice.reducer,
+	user: userSlice.reducer,
 });
 
 export default reducer;
