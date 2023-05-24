475811cc79a97801552dc256ec823d73ebfaa3ac
Nischal-Ad <92177440+Nischal-Ad@users.noreply.github.com>
2023-05-24 09:10:19 +0545

    eslint and prittier setup

--- frontend/src/common/utils/localAxios.ts
+++ frontend/src/common/utils/localAxios.ts
@@ -1,23 +1,23 @@
-import axios from 'axios';
+import axios from 'axios'
 
 const localAxios = axios.create({
-	baseURL: 'http://localhost:3000',
-	withCredentials: true,
-});
+  baseURL: 'http://localhost:3000',
+  withCredentials: true,
+})
 
-localAxios.interceptors.response.use((response) => response.data);
+localAxios.interceptors.response.use((response) => response.data)
 
 localAxios.interceptors.request.use((config) => {
-	const [pathname, queries] = config.url?.split('?') || '';
+  const [pathname, queries] = config.url?.split('?') || ''
 
-	const q = new URLSearchParams(queries);
+  const q = new URLSearchParams(queries)
 
-	const apiKey = localStorage.getItem('apiKey');
-	if (apiKey) q.set('apiKey', apiKey);
+  const apiKey = localStorage.getItem('apiKey')
+  if (apiKey) q.set('apiKey', apiKey)
 
-	config.url = `${pathname}?${q.toString()}`;
+  config.url = `${pathname}?${q.toString()}`
 
-	return config;
-});
+  return config
+})
 
-export default localAxios;
+export default localAxios
