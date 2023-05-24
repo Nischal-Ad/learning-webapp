863c5cd1e2a87f0ba195ddefb17b83fc45d8cb89
Nischal-Ad <92177440+Nischal-Ad@users.noreply.github.com>
2023-05-24 17:10:46 +0545

    prittier change

--- frontend/src/redux/slices/user.slice.ts
+++ frontend/src/redux/slices/user.slice.ts
@@ -1,38 +1,38 @@
-import { createSlice } from '@reduxjs/toolkit';
-import type { CaseReducer, PayloadAction } from '@reduxjs/toolkit';
+import { createSlice } from '@reduxjs/toolkit'
+import type { CaseReducer, PayloadAction } from '@reduxjs/toolkit'
 
 const initialState = {
-	status: 'idle',
-	data: {} as any,
-	error: '',
-};
+  status: 'idle',
+  data: {} as any,
+  error: '',
+}
 
 const setLoading: CaseReducer<any> = (state) => ({
-	...state,
-	status: 'loading',
-	error: '',
-});
+  ...state,
+  status: 'loading',
+  error: '',
+})
 
 const setData: CaseReducer<any, PayloadAction<any>> = (state, action) => ({
-	status: 'success',
-	data: action.payload,
-	error: '',
-});
+  status: 'success',
+  data: action.payload,
+  error: '',
+})
 
 const setError: CaseReducer<any, PayloadAction<any>> = (state, action) => ({
-	...state,
-	status: 'error',
-	error: action.payload,
-});
+  ...state,
+  status: 'error',
+  error: action.payload,
+})
 
 const userSlice = createSlice({
-	name: 'user',
-	initialState: initialState,
-	reducers: {
-		setLoading,
-		setData,
-		setError,
-	},
-});
+  name: 'user',
+  initialState: initialState,
+  reducers: {
+    setLoading,
+    setData,
+    setError,
+  },
+})
 
-export default userSlice;
+export default userSlice
