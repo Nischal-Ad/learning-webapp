d5c2cf8a0239bef4aaafd5d7d04916e3f55c3fc7
Anup Kasula <anup.kasula@elscript.com>
2023-06-04 21:37:50 +0545

    course module

--- backend/src/controllers/coursesController.ts
+++ backend/src/controllers/coursesController.ts
@@ -1,16 +1,22 @@
 import catchAsync from '@Middleware/catchAsync'
 import ErrorHandler from '@Utils/errorHandler'
 import courseModel, { TCourse } from '@Models/courseModel'
+import GetAll from '@Components/getAll'
+import GetOne from '@Components/getOne'
+import Update from '@Components/updateOne'
+import Delete from '@Components/deleteOne'
 
 export const Createcourse = catchAsync(async (req, res, next) => {
-  const { name, description }: TCourse = req.body
+  const { course_title, course_category, topic_id, enroll }: TCourse = req.body
 
-  if (!name || !description)
+  if (!course_title || !course_category || !topic_id || !enroll)
     return next(new ErrorHandler('Please enter all field', 400))
 
   const newCourse: TCourse = await courseModel.create({
-    name,
-    description,
+    course_title,
+    course_category,
+    topic_id,
+    enroll,
   })
 
   res.status(201).json({
@@ -19,3 +25,11 @@ export const Createcourse = catchAsync(async (req, res, next) => {
     data: newCourse,
   })
 })
+
+export const getAllCourse = GetAll(courseModel)
+
+export const getoneCourse = GetOne(courseModel, 'Course')
+
+export const updateoneCourse = Update(courseModel, 'Course')
+
+export const deleteoneCourse = Delete(courseModel, 'Course')
--- backend/src/models/courseModel.ts
+++ backend/src/models/courseModel.ts
@@ -3,17 +3,25 @@ import { InferSchemaType, Schema, model } from 'mongoose'
 export type TCourse = InferSchemaType<typeof courseSchema>
 
 const courseSchema = new Schema({
-  name: {
+  course_title: {
     type: String,
     required: [true, 'please enter your name'],
     minLength: [4, 'name must be at least 4 characters'],
     maxLength: [20, 'name cannot exceed 20 characters'],
   },
 
-  description: {
+  course_category: {
     type: String,
-    required: [true, 'Please enter your desc'],
+    required: [true, 'Please enter your course_category'],
+  },
+  topic_id: {
+    type: Number,
+    required: [true, 'Please enter your topic_id'],
+  },
+  enroll: {
+    type: String,
+    required: [true, 'Please enter your enroll'],
   },
 })
 
-export default model<TCourse>('course', courseSchema)
+export default model<TCourse>('courses', courseSchema)
