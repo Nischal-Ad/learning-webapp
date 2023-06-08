13a28e23000ee6feec9e9e5744f574444272942d
Nischal-Ad <92177440+Nischal-Ad@users.noreply.github.com>
2023-06-08 15:47:36 +0545

    added auth to logout route

--- backend/src/routes/authRoute.ts
+++ backend/src/routes/authRoute.ts
@@ -15,13 +15,13 @@ const router = express.Router()
 
 router.route('/register/user').post(RegisterUser)
 router.route('/login').post(LoginUser)
-router.route('/logout').get(Logout)
 
 router.route('/forgotpassword').post(forgetPassword)
 router.route('/resetpassword/:token').put(resetPassword)
 
 // Protected routes only for logged in user
 router.route('/changepassword').put(isAuth, ChangePassword)
+router.route('/logout').get(isAuth, Logout)
 
 //protected routes only for teachers
 
