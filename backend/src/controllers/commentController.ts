a03d10a562cf5acc6d2afeb56829a27a3b59ec19
Anup Kasula <anup.kasula@elscript.com>
2023-06-15 20:23:23 +0545

    change in comment module

--- backend/src/controllers/commentController.ts
+++ backend/src/controllers/commentController.ts
@@ -2,14 +2,18 @@ import catchAsync from '@Middleware/catchAsync'
 import commentModel, { TComment } from '@Models/commentModel'
 
 import GetOne from '@Components/getOne'
+import GetAll from '@Components/getAll'
+import Update from '@Components/updateOne'
+import Delete from '@Components/deleteOne'
 
 export const Createcomment = catchAsync(async (req, res) => {
-  const { course_comment, course_id, user_id }: TComment = req.body
+  const { course_comment, course_id, user_id,rating }: TComment = req.body
 
   const newComment: TComment = await commentModel.create({
     course_comment,
     course_id,
     user_id,
+    rating
   })
 
   res.status(201).json({
@@ -20,3 +24,10 @@ export const Createcomment = catchAsync(async (req, res) => {
 })
 
 export const getonecomment = GetOne(commentModel, 'comments')
+
+export const getAllComment = GetAll(commentModel)
+
+
+export const updateoneComment = Update(commentModel, 'comments')
+
+export const deleteoneCoomment = Delete(commentModel, 'comments')
