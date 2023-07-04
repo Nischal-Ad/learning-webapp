745301303debeda88d7c1cc2effcb8d855d15987
Nischal-Ad <92177440+Nischal-Ad@users.noreply.github.com>
2023-07-04 18:25:59 +0545

    created filter with sort and price in courses

--- frontend/src/features/user/courses/index.tsx
+++ frontend/src/features/user/courses/index.tsx
@@ -1,15 +1,28 @@
 import Helmet from '@Components/Helmet'
 import Section from '@Components/SectionWrapper'
-import { Pagination, Stack, Typography } from '@mui/material'
+import {
+  Pagination,
+  Stack,
+  Typography,
+  Select,
+  MenuItem,
+  TextField,
+  IconButton,
+} from '@mui/material'
 import CourseCard from '@Components/Courses/Index'
 import { useCourse } from './hooks/useCourse'
 import { useAppSelector } from '@Store'
 import Loading from '@Components/Loader'
-import { useEffect } from 'react'
+import { useEffect, useState } from 'react'
+import { SelectChangeEvent } from '@mui/material'
+import SmartDisplayRoundedIcon from '@mui/icons-material/SmartDisplayRounded'
 import { useSearchParams } from 'react-router-dom'
 
 const Index = () => {
   const { ongetAllCourses } = useCourse()
+  const [sort, setSort] = useState('-ratings')
+  const [min, setMin] = useState('')
+  const [max, setMax] = useState('')
   const { status, data } = useAppSelector((store) => store.course)
   const [params, setParams] = useSearchParams()
 
@@ -22,9 +35,56 @@ const Index = () => {
     })
   }
 
+  const handelSort = (e: SelectChangeEvent<string>) => {
+    setSort(e.target.value as string)
+    setParams((prev) => {
+      const params = prev
+      params.set('sort', e.target.value)
+
+      return params
+    })
+  }
+
+  const handelPrice = () => {
+    if (max && min) {
+      setParams((prev) => {
+        const params = prev
+        params.set('price[gte]', min.toString())
+        params.set('price[lte]', max.toString())
+
+        return params
+      })
+    } else if (max) {
+      setParams((prev) => {
+        const params = prev
+        params.set('price[lte]', max.toString())
+
+        return params
+      })
+    } else if (min) {
+      setParams((prev) => {
+        const params = prev
+        params.set('price[gte]', min.toString())
+
+        return params
+      })
+    } else {
+      setParams((prev) => {
+        const params = prev
+        params.delete('price[gte]')
+        params.delete('price[lte]')
+
+        return params
+      })
+    }
+  }
+
   const searchParams = params.toString().replace(/\+/g, ' ')
 
   useEffect(() => {
+    if (!params.has('sort')) {
+      ongetAllCourses(`${searchParams}&sort=-ratings`)
+    }
     ongetAllCourses(searchParams)
   }, [searchParams])
   return (
@@ -39,6 +99,62 @@ const Index = () => {
         <Section id="courses">
           {data && Array.isArray(data?.data) && data.data.length !== 0 ? (
             <>
+              <Stack justifyContent={'space-between'} direction={'row'} pb={1.5}>
+                <Stack
+                  direction={{ sm: 'row', xs: 'column' }}
+                  spacing={{ sm: 1 }}
+                  alignItems={'center'}
+                  sx={{ input: { padding: 1, width: '2.5rem' } }}
+                >
+                  <Typography variant="body2">Price:</Typography>
+                  <Stack alignItems={'center'} direction={'row'} spacing={1}>
+                    <TextField
+                      id="min"
+                      type="number"
+                      size="small"
+                      placeholder="min"
+                      value={min}
+                      onChange={(e) => setMin(e.target.value)}
+                    />
+                    <TextField
+                      id="max"
+                      type="number"
+                      size="small"
+                      placeholder="max"
+                      value={max}
+                      onChange={(e) => setMax(e.target.value)}
+                    />
+                    <IconButton
+                      aria-label="SmartDisplayRoundedIcon"
+                      onClick={handelPrice}
+                      sx={{
+                        width: '3rem',
+                        height: '3rem',
+                      }}
+                    >
+                      <SmartDisplayRoundedIcon
+                        color="primary"
+                        sx={{
+                          width: '3rem',
+                          height: '3rem',
+                        }}
+                      />
+                    </IconButton>
+                  </Stack>
+                </Stack>
+                <Stack direction={{ sm: 'row', xs: 'column' }} spacing={1} alignItems={'center'}>
+                  <Typography variant="body2">Sort By:</Typography>
+                  <Select id="sort" value={sort} onChange={handelSort} size="small">
+                    <MenuItem value={'-ratings'}>
+                      <em>Default</em>
+                    </MenuItem>
+                    <MenuItem value={'ratings'}>Ratings low to high</MenuItem>
+                    <MenuItem value={'price'}>Price low to high</MenuItem>
+                    <MenuItem value={'-price'}>Price high to low</MenuItem>
+                  </Select>
+                </Stack>
+              </Stack>
+
               <Stack spacing={1} direction={'column'}>
                 {data?.data?.map((course, i) => {
                   if (course) {
